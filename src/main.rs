3e80b0d4b9598f4b6fd3390433254fbee44bdb3a
davisuga <daviciencia1@gmail.com>
2023-04-27 13:43:59 -0300

    Fixed event handling

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,76 @@
-use crate::zillow_scrapper::get_profile_data;
+use core::fmt;
+
+use crate::zillow_scrapper::{get_profile_data, get_profile_data_with_redis_cache};
 use futures::TryFutureExt;
-use lambda_runtime::{service_fn, Error, LambdaEvent};
-use serde::{Deserialize, Serialize};
+use lambda_http::{Body, Error, IntoResponse, Request, RequestExt, Response};
+use serde::Deserialize;
 use tracing::info;
 
 pub mod zillow_profile;
 pub mod zillow_scrapper;
 #[derive(Deserialize)]
-struct Request {
-    body: String,
-}
-
-#[derive(Deserialize)]
 struct AgentInfo {
     username: String,
 }
 
-#[derive(Debug, Serialize)]
-struct Response {
-    req_id: String,
-    body: String,
-    #[serde(rename = "statusCode")]
-    status_code: u16,
-    cookies: String,
-}
+#[derive(Debug)]
+struct CustomError(String);
 
-impl std::fmt::Display for Response {
-    /// Display the response struct as a JSON string
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let err_as_json = serde_json::json!(self).to_string();
-        write!(f, "{err_as_json}")
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
 
-impl std::error::Error for Response {}
-async fn deserialize_agent_info(
-    req_id: String,
-    body: String,
-) -> Result<AgentInfo, Box<dyn std::error::Error + Send + Sync>> {
-    serde_json::from_str(&body).map_err(|err| {
-        info!("Failed to parse input: {:?}", err);
-        Box::new(Response {
-            req_id,
-            body: "Failed to parse input".to_owned(),
-            status_code: 500,
-            cookies: String::new(),
-        }) as Box<dyn std::error::Error + Send + Sync>
-    })
+impl std::error::Error for CustomError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
 }
 
-async fn fetch_profile_data(username: String, req_id: String) -> Result<Response, Error> {
-    get_profile_data(&username)
-        .await
-        .map(|profile| {
-            info!("profile: {:?}", &profile);
-            Response {
-                req_id: req_id.clone(),
-                body: serde_json::to_string(&profile).expect("Failed to serialize profile"),
-                status_code: 200,
-                cookies: String::new(),
+async fn fetch_agent_data(event: Request) -> Result<impl IntoResponse, Error> {
+    let body = event.body().to_owned();
+    info!("body: {:?}", &body);
+    match &body {
+        Body::Empty | Body::Binary(_) => {
+            Err(Box::new(CustomError("Failed to parse input: {}".to_owned())) as Error)
+        }
+        Body::Text(body) => {
+            let agent_info = serde_json::from_str::<AgentInfo>(&body)?;
+
+            match get_profile_data_with_redis_cache(&agent_info.username).await {
+                Err(_) => Ok(Response::builder()
+                    .status(500)
+                    .body::<String>("Failed to fetch profile".into())
+                    .expect("failed to build response")),
+
+                Ok(profile) => {
+                    info!("profile: {:?}", &profile);
+                    let response_body =
+                        serde_json::to_string(&profile).expect("Failed to serialize profile");
+
+                    let resp = Response::builder()
+                        .status(200)
+                        .header("content-type", "application/json")
+                        .body(response_body.into())
+                        .expect("Failed to build response");
+
+                    Ok(resp)
+                }
             }
-        })
-        .map_err(|err| {
-            info!("Failed to parse: {:?}", err);
-            Box::new(Response {
-                req_id,
-                body: "Failed to parse input".to_owned(),
-                status_code: 500,
-                cookies: String::new(),
-            }) as Box<dyn std::error::Error + Send + Sync>
-        })
+        }
+    }
 }
 
-#[tracing::instrument(skip(event), fields(req_id = %event.context.request_id))]
-async fn fetch_agent_data(event: LambdaEvent<Request>) -> Result<Response, Error> {
-    info!("handling a request");
-
-    deserialize_agent_info(event.context.request_id.clone(), event.payload.body)
-        .and_then(|agent_info| fetch_profile_data(agent_info.username, event.context.request_id))
-        .await
-}
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
         .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
+    info!("starting lambda");
+    lambda_http::run(lambda_http::service_fn(fetch_agent_data)).await?;
 
-    // Initialize the client here to be able to reuse it across
-    // different invocations.
-    //
-    // No extra configuration is needed as long as your Lambda has
-    // the necessary permissions attached to its role.
-    // let config = aws_config::load_from_env().await;
-
-    lambda_runtime::run(service_fn(|event: LambdaEvent<Request>| async {
-        fetch_agent_data(event).await
-    }))
-    .await
+    Ok(())
 }
